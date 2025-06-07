e650a9f12559c522545bb2ea4da7aa7b245c7446
ak_r <ak_r@git.com>
2025-06-07 19:34:46 +0900

    ログインログアウト実装できた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
-class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
-
-        skip_before_action :verify_authenticity_token
-        helper_method :current_api_v1_user, :user_signed_in?
+class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken 
+        before_action do
+                I18n.locale = :ja
+        end
 end
